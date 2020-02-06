32ea1fd75f9c6f08646ed039f033ae483fc30fd4
Trevor Sawler <trevor.sawler@gmail.com>
2020-02-06 17:37:58 -0400

    Page access level

--- client-handlers.go
+++ client-handlers.go
@@ -1,28 +1,9 @@
 package clienthandlers
 
 import (
-	"database/sql"
 	"fmt"
-	"github.com/tsawler/goblender/client/clienthandlers/clientdb"
-	"github.com/tsawler/goblender/client/clienthandlers/clientmodels"
-	"github.com/tsawler/goblender/pkg/config"
 	"net/http"
-	"time"
 )
-
-func NewClientPageHandlers(conn *sql.DB, app config.AppConfig) {
-	pageModel = &clientdb.PageModel{DB: conn}
-}
-
-func TestDB(w http.ResponseWriter, r *http.Request) {
-	results, err := pageModel.AllPages()
-	if err != nil {
-		fmt.Println(err)
-	}
-	for _, x := range results {
-		fmt.Println("Page:", x.Slug)
-	}
-}
 
 func TestHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
@@ -30,13 +11,5 @@ func TestHandler(w http.ResponseWriter, r *http.Request) {
 
 
 func TestProtectedHandler(w http.ResponseWriter, r *http.Request) {
-	example := clientmodels.Sample{
-		ID:        1,
-		Name:      "Jack",
-		Phone:     "506-444-5555",
-		CreatedAt: time.Now(),
-		UpdatedAt: time.Now(),
-	}
-
-	w.Write([]byte(fmt.Sprintf("It worked, and name is %s", example.Name)))
+	fmt.Println("Worked!")
 }
--- client-routes.go
+++ client-routes.go
@@ -3,13 +3,19 @@ package clienthandlers
 import (
 	"github.com/bmizerany/pat"
 	"github.com/justinas/alice"
-	"github.com/tsawler/goblender/client/clienthandlers/clientdb"
 	"github.com/tsawler/goblender/pkg/config"
+	"github.com/tsawler/goblender/pkg/driver"
 	"github.com/tsawler/goblender/pkg/middleware"
+	"github.com/tsawler/goblender/pkg/repository"
+    "github.com/tsawler/goblender/pkg/repository/page"
+    "log"
 )
 
 var app config.AppConfig
-var pageModel *clientdb.PageModel
+var parentDB *driver.DB
+var pageModel repository.PageRepo
+var infoLog *log.Logger
+var errorLog *log.Logger
 
 // ClientRoutes is used to handle custom routes for specific clients. Prepend some unique (and site wide) value
 // to the start of each route in order to avoid clashes with pages, etc. Middleware can be applied by importing and
@@ -18,14 +24,15 @@ func ClientRoutes(mux *pat.PatternServeMux, standardMiddleWare, dynamicMiddlewar
 
 	mux.Get("/custom/my-test-route", standardMiddleWare.ThenFunc(TestHandler))
 	mux.Get("/custom/protected-route", standardMiddleWare.Append(middleware.Auth).ThenFunc(TestProtectedHandler))
-	mux.Get("/custom/db", standardMiddleWare.Append(middleware.Auth).ThenFunc(TestDB))
 
 	return mux, nil
 }
 
 // ClientInit gives us access to site values for client code.
-func ClientInit(c config.AppConfig) {
-	app = c
-	conn := app.Connections["NBTAP"]
-	pageModel = &clientdb.PageModel{DB: conn}
+func ClientInit(c config.AppConfig, p *driver.DB) {
+    app = c
+    infoLog = app.InfoLog
+    errorLog = app.ErrorLog
+    pageModel = page.NewSQLPageRepo(p.SQL)
+    parentDB = p
 }
