bd388466e276edc9faf1e6f9901ff9411a37328d
Trevor Sawler <trevor.sawler@gmail.com>
2020-02-08 16:57:24 -0400

    Update handler/routes for additional integration with goblender

--- client-handlers.go
+++ client-handlers.go
@@ -1,15 +1,24 @@
 package clienthandlers
 
 import (
-	"fmt"
 	"net/http"
 )
 
-func TestHandler(w http.ResponseWriter, r *http.Request) {
-	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
+// JSONResponse is a generic struct to hold json responses
+type JSONResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
 }
 
+// ShowHome returns the home page using our local page template for the client
+func ShowHome(w http.ResponseWriter, r *http.Request) {
+	pageHandlers.SetDefaultPageTemplate("page.page.tmpl")
+	pageHandlers.Home(w, r)
+}
 
-func TestProtectedHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Worked!")
+// other routes as necessary e.g.
+
+// SomeHandler gets all motorcycles
+func SomeHandler(w http.ResponseWriter, r *http.Request) {
+	// ... do whatever
 }
--- client-routes.go
+++ client-routes.go
@@ -5,34 +5,55 @@ import (
 	"github.com/justinas/alice"
 	"github.com/tsawler/goblender/pkg/config"
 	"github.com/tsawler/goblender/pkg/driver"
-	"github.com/tsawler/goblender/pkg/middleware"
+	"github.com/tsawler/goblender/pkg/handlers"
 	"github.com/tsawler/goblender/pkg/repository"
-    "github.com/tsawler/goblender/pkg/repository/page"
-    "log"
+	"github.com/tsawler/goblender/pkg/repository/page"
+	"log"
+	"net/http"
 )
 
 var app config.AppConfig
-var parentDB *driver.DB
-var pageModel repository.PageRepo
 var infoLog *log.Logger
 var errorLog *log.Logger
+var pageModel repository.PageRepo
+var parentDB *driver.DB
+
+var preferenceHandlers *handlers.PreferenceDBRepo
+var pageHandlers *handlers.PageDBRepo
+var userHandlers *handlers.UserDBRepo
+var roleHandlers *handlers.RoleDBRepo
+var historyHandlers *handlers.HistoryDBRepo
+var postHandlers *handlers.PostDBRepo
 
 // ClientRoutes is used to handle custom routes for specific clients. Prepend some unique (and site wide) value
 // to the start of each route in order to avoid clashes with pages, etc. Middleware can be applied by importing and
-// using the middleware.* functions
+// using the middleware.* functions. All handlers are available in appHandlers, e.g. to use pageHandlers, appHandlers.PageHandlers
 func ClientRoutes(mux *pat.PatternServeMux, standardMiddleWare, dynamicMiddleware alice.Chain) (*pat.PatternServeMux, error) {
 
-	mux.Get("/custom/my-test-route", standardMiddleWare.ThenFunc(TestHandler))
-	mux.Get("/custom/protected-route", standardMiddleWare.Append(middleware.Auth).ThenFunc(TestProtectedHandler))
+	mux.Get("/", dynamicMiddleware.ThenFunc(ShowHome))
+
+	mux.Get("/some-handler", standardMiddleWare.ThenFunc(SomeHandler))
+
+	// public folder
+	fileServer := http.FileServer(http.Dir("./client/clienthandlers/public/"))
+	mux.Get("/client/static/", http.StripPrefix("/client/static", fileServer))
 
 	return mux, nil
 }
 
 // ClientInit gives us access to site values for client code.
 func ClientInit(c config.AppConfig, p *driver.DB) {
-    app = c
-    infoLog = app.InfoLog
-    errorLog = app.ErrorLog
-    pageModel = page.NewSQLPageRepo(p.SQL)
-    parentDB = p
+	app = c
+	//conn := app.Connections["wheels"]
+	infoLog = app.InfoLog
+	errorLog = app.ErrorLog
+	pageModel = page.NewSQLPageRepo(p.SQL)
+	parentDB = p
+
+	preferenceHandlers = handlers.NewPreferenceHandlers(p)
+	historyHandlers = handlers.NewHistoryHandler(p)
+	roleHandlers = handlers.NewRoleHandlers(p, historyHandlers)
+	userHandlers = handlers.NewUserHandlers(app, p, roleHandlers)
+	pageHandlers = handlers.NewPageHandler(app, p, userHandlers, preferenceHandlers)
+	postHandlers = handlers.NewPostHandlers(app, p, pageHandlers)
 }
