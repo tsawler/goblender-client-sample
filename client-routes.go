8eab2993f8647a3b98870b2dee311739c2243aea
Trevor Sawler <trevor.sawler@gmail.com>
2020-02-10 18:27:59 -0400

    Update comments

--- client-routes.go
+++ client-routes.go
@@ -8,10 +8,10 @@ import (
 
 // ClientRoutes is used to handle custom routes for specific clients. Prepend some unique (and site wide) value
 // to the start of each route in order to avoid clashes with pages, etc. Middleware can be applied by importing and
-// using the middleware.* functions. All handlers are available in appHandlers, e.g. to use pageHandlers, appHandlers.PageHandlers
+// using the middleware.* functions.
 func ClientRoutes(mux *pat.PatternServeMux, standardMiddleWare, dynamicMiddleware alice.Chain) (*pat.PatternServeMux, error) {
 
-	// we can override routes in goblender, if we wish, e.g.
+	// we can override routes in goblender, if we wish, assuming we init the handlers in client-init.go
 	//mux.Get("/", dynamicMiddleware.ThenFunc(pageHandlers.Home))
 
 	mux.Get("/client/some-handler", standardMiddleWare.ThenFunc(SomeHandler))
