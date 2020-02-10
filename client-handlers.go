ef2a33ddfc85716c68fd842b2c5b7b6856b4a962
Trevor Sawler <trevor.sawler@gmail.com>
2020-02-10 18:24:41 -0400

    Remove unused code

--- client-handlers.go
+++ client-handlers.go
@@ -11,10 +11,12 @@ func SomeHandler(w http.ResponseWriter, r *http.Request) {
 	helpers.Render(w, r, "client-sample.page.tmpl", &templates.TemplateData{})
 }
 
-// ShowHome is a sample handler which returns the home page using our local page template for the client,
+// The next func is a sample handler which returns the home page using our local page template for the client,
 // and is called from client-routes.go using a route that overrides the one in goblender. This allows us
 // to build custom functionality without having to use non-standard routes.
 // **Note** that the template below *doesn't actually exist* in this repo ,and this handler is never really called.
+
+// ShowHome shows home page using custom template, and overrides the route in default goblender application
 //func ShowHome(w http.ResponseWriter, r *http.Request) {
 //	pageHandlers.SetDefaultPageTemplate("custom-home--page.page.tmpl")
 //	pageHandlers.Home(w, r)
