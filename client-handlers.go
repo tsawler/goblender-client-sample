988f34e168bdd767549643c11f1cad56fccc449c
Trevor Sawler <trevor.sawler@gmail.com>
2020-06-07 13:37:00 -0300

    Spelling

--- client-handlers.go
+++ client-handlers.go
@@ -11,7 +11,7 @@ func SomeHandler(w http.ResponseWriter, r *http.Request) {
 	helpers.Render(w, r, "client-sample.page.tmpl", &templates.TemplateData{})
 }
 
-// The next func is a sample handler which returns the home page using our local page template for the client,
+// The next function is a sample handler which returns the home page using our local page template for the client,
 // and is called from client-routes.go using a route that overrides the one in goblender. This allows us
 // to build custom functionality without having to use non-standard routes.
 func CustomShowHome(w http.ResponseWriter, r *http.Request){
